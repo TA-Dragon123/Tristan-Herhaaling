bca792e4b3737c953efeb71940a0870a55ebe4d6
TA-Dragon123 <tristan.ansingh@bazandpoort.be>
2025-09-15 11:21:10 +0200

    php2

--- controle.php
+++ controle.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 $_SESSION["gebruiker"] = "";
+$_SESSION["wachtwoord"] = "";
+if (isset($_POST["naam"])) $_SESSION["gebruiker"] = $_POST["naam"];
+if (isset($_POST["wachtwoord"])) $_SESSION["wachtwoord"] = $_POST["wachtwoord"];
 include "connect.php";
 ?>
 <!DOCTYPE html>
@@ -13,12 +16,24 @@ include "connect.php";
 <body>
     <?php
     if (isset($_POST["knop"])) {
-        $sql = "SELECT wachtwoord, naam FROM tblgebruikers WHERE naam ='".$_SESSION["gebruiker"]."' and wachtwoord  ='".$_SESSION['wachtwoord']."'";
+        $sql = "SELECT wachtwoord, naam, Klas FROM tblgebruiker WHERE naam ='".$_POST["naam"]."' and wachtwoord  ='".$_POST['wachtwoord']."'";
         $resultaat = $mysqli->query($sql);
         $rowAantal = $resultaat->num_rows;
-        if $rowAantal = 1 {
-            header ('Location: Leerling.php');
-        }
+        if ($rowAantal == 1) {
+            $rij = $resultaat->fetch_assoc();
+            if ($rij["Klas"]  == "LKR") {
+                $_SESSION["leerkracht"] = "ja";
+                header ('Location: Leerkracht.php');
+                exit;
+            } else if ($rij["Klas"]  == "6AD") {
+                $_SESSION["leerkracht"] = "nee";
+                header ('Location: Leerling.php');
+                exit;
+            } else {
+                echo "  <h2>Je bent niet ingelogd <br>
+                        <a href='index.php'>Probeer opnieuw</a></h2>";
+    }
+}
     }
     ?>
 </body>
